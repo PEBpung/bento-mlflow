b46dfee00f67fb9d8a89e4d7766e8de8e3b06734
pebpung <mcuwwa29@gmail.com>
2022-01-10 23:19:00 +0900

    feat(run) : mlflow initialize

---
+++ run.py
@@ -0,0 +1,15 @@
+from sklearn import svm
+from sklearn import datasets
+import mlflow
+
+# Load training data
+iris = datasets.load_iris()
+X, y = iris.data, iris.target
+
+# Model Training and saving in MLflow
+clf = svm.SVC(gamma="scale")
+with mlflow.start_run() as mlflow:
+    clf.fit(X, y)
+    mlflow.sklearn.log_model(
+        sk_model=clf, artifact_path="model", signature=mlflow.models.signature.infer_signature(X),
+    )
