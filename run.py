85176440563d7d30c225d25653c8bec2cc3ddf5c
pebpung <mcuwwa29@gmail.com>
2022-01-10 23:22:37 +0900

    fix(mlflow) : mlflow->run 오타 수정

--- run.py
+++ run.py
@@ -8,7 +8,7 @@ X, y = iris.data, iris.target
 
 # Model Training and saving in MLflow
 clf = svm.SVC(gamma="scale")
-with mlflow.start_run() as mlflow:
+with mlflow.start_run() as run:
     clf.fit(X, y)
     mlflow.sklearn.log_model(
         sk_model=clf, artifact_path="model", signature=mlflow.models.signature.infer_signature(X),
